aef5552570d05bf68432b2ecefe0fada867bb4f6
unknown <pitzertad@gmail.com>
2017-07-22 21:35:00 -0700

    reads file for url works

--- augChallenge.py
+++ augChallenge.py
@@ -1,11 +1,22 @@
-
+import os
 from bs4 import BeautifulSoup
 import requests
 
-url = "http://google.com"
+
+#reads file for url
+for file in os.listdir('.\datafolder'):
+	with open('./datafolder/' + file, 'r') as f:
+		fcontents = f.read()
+		# print (fcontents)
+		url = fcontents
+
+#prints title string for writing
+# url = "http://google.com"
 response = requests.get(url)
 html = response.content
 soup = BeautifulSoup(html, 'html.parser')
 titleString = soup.title.string
 
 print (titleString)
+
+
