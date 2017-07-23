80e8acbfc9b93a6bf0d3566dd7311a35bbdd1c4d
unknown <pitzertad@gmail.com>
2017-07-23 00:40:18 -0700

    still done, needs oop

--- augChallenge.py
+++ augChallenge.py
@@ -2,15 +2,6 @@ import os
 import requests
 import datetime
 from bs4 import BeautifulSoup
-'''
-open a file that has contains a website path, but it lacks the protocol example: google.com, not http://google.com				check
-extract the title of the page from the meta data provided in the page, the stuff between the tag <title></title>				check
-write that title in a new file called website name minus the protocol or any additional path information (example: google)		check
- the file contents should be the full path (take special note here, you might not get a top of domain page)
- the title of the page
- the time of the of the writing of the data in day/month/year 24:00:00 (no .seconds and year can be 2 or 4 digits, your choice)	check
- lastly your porn star name (name of your childhood pet + street you grew up on) or just your name.								check
-'''
 
 #reads file for url
 for file in os.listdir('.\datafolder'):
@@ -47,9 +38,9 @@ if filterDomainOnly[-1] == 'org' or 'com':
 
 os.chdir('.\datafolder')
 with open(ofilename + '.txt', 'w') as f:
-	# print (ofilename)
-	f.write(fcontents + '\n') 
-	f.write(titleString + '\n')
-	f.write(modTime + '\n')
-	f.write(pornStarName)
+	answer = '%s\n%s\n%s\n%s' % (fcontents, titleString, modTime, pornStarName)
+	f.write(answer) 
+	# f.write(titleString + '\n')
+	# f.write(modTime + '\n')
+	# f.write(pornStarName)
 	f.close()
